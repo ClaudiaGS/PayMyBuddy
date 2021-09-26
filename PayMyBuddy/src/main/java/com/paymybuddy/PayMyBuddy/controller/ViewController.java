d61d56b9df2ec39a56f92da0e6df319a1bd2694e
ClaudiaGugiuman <claudiagugiuman@yahoo.com>
2021-09-26 17:51:25 +0200

    Contact Management Vue added

--- PayMyBuddy/src/main/java/com/paymybuddy/PayMyBuddy/controller/ViewController.java
+++ PayMyBuddy/src/main/java/com/paymybuddy/PayMyBuddy/controller/ViewController.java
@@ -1,7 +1,6 @@
 package com.paymybuddy.PayMyBuddy.controller;
 
-import com.paymybuddy.PayMyBuddy.model.Account;
-import com.paymybuddy.PayMyBuddy.model.UserComplete;
+import com.paymybuddy.PayMyBuddy.model.*;
 import com.paymybuddy.PayMyBuddy.service.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,10 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ViewController {
@@ -36,6 +39,7 @@ public class ViewController {
     
     @GetMapping("/login")
     public String connexion(Model model, Account account) {
+        
         model.addAttribute("account", account);
         return "Login";
     }
@@ -62,8 +66,64 @@ public class ViewController {
     
     @GetMapping("/homePage")
     public String homePage( Model model) {
-        model.addAttribute("user", (UserComplete)session.getAttribute("userComplete"));
-        return "Home";
+
+            
+                model.addAttribute("user", (UserComplete)session.getAttribute("userComplete"));
+                return "Home";
+
+        
     }
+    
+    // CONTACT LIST MANAGEMENT:
+    @GetMapping("/contacts")
+    public String contactView(Model model) {
+        Iterable<ContactView> contactViewList = contactViewService.getContactViewList((int)session.getAttribute("userIDAccount"));
+        model.addAttribute("contacts", contactViewList);
+        return "Contacts";
+    }
+    
+    @GetMapping("/deleteContact/{id}")
+    public ModelAndView deleteContact(@PathVariable("id") final int id) {
+        logger.info("delete contact with id  " + id + " in View Controller");
+        contactService.deleteContact(id);
+        return new ModelAndView("redirect:/contacts");
+    }
+    
+  
+    @GetMapping("/createContact")
+    public String createContact(Model model) {
+        ContactView contactView = new ContactView();
+        List<ContactView> contactViewList = contactViewService.getContactViewList((int)session.getAttribute("userIDAccount"));
+        List<String> contactViewMails = new ArrayList<String>();
+        for (ContactView cv : contactViewList) {
+            contactViewMails.add(cv.getEmail());
+        }
+        logger.info(contactViewMails);
+        List<String> emailList = new ArrayList<String>();
+        for (Account account : accountService.readAccountList()) {
+            if (account.getUserID() != ((int)session.getAttribute("userIDAccount")) && !(contactViewMails.contains(account.getAccountEmail()))) {
+                emailList.add(account.getAccountEmail());
+            }
+        }
+        model.addAttribute("contact", contactView);
+        model.addAttribute("emails", emailList);
+        return "addContact";
+    }
+    
+    @PostMapping("/saveContact")
+    public ModelAndView saveContact(@ModelAttribute ContactView contact) {
+        
+        List<Account> accountList = accountService.readAccountList();
+        
+        int userIDContact = 0;
+        for (Account account : accountList) {
+            if (account.getAccountEmail().equals(contact.getEmail())) {
+                userIDContact = account.getUserID();
+            }
+        }
+        contactService.createContact((int)session.getAttribute("userIDAccount"), userIDContact);
+        return new ModelAndView("redirect:/contacts");
+    }
+    
    
 }
