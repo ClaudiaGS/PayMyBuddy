4048219cd605822ae814e2cfcf1fe89527da2700
ClaudiaGugiuman <claudiagugiuman@yahoo.com>
2021-09-26 18:35:47 +0200

    Data.sql modified

--- PayMyBuddy/src/main/resources/Data.sql
+++ PayMyBuddy/src/main/resources/Data.sql
@@ -52,15 +52,35 @@ FOREIGN KEY (USER_ID_CONTACT) REFERENCES user(USER_ID)
 insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname1','lastname1','2001-01-01');
 insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname2','lastname2','2002-02-02');
 insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname3','lastname3','2003-03-03');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname4','lastname4','2004-04-04');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname5','lastname5','2005-05-05');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname6','lastname6','2006-06-06');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname7','lastname7','2007-07-07');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname8','lastname8','2008-08-08');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname9','lastname9','2009-09-09');
+insert into user(USER_FIRST_NAME,USER_LAST_NAME,USER_BIRTHDATE) values('firstname10','lastname10','2010-10-10');
 
-insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('333333333','11.1','EURO','3');
-insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('222222222','22.2','EURO','2');
-insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('111111112','52.2','EURO','1');
+
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('11111','1000','EURO','1');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('22222','2000','EURO','2');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('33333','3000','EURO','3');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('44444','4000','EURO','4');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('55555','5000','EURO','5');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('66666','6000','EURO','6');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('77777','7000','EURO','7');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('88888','8000','EURO','8');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('99999','9000','EURO','9');
+insert into bank_account(BANK_ACCOUNT_NUMBER,BANK_ACCOUNT_AMOUNT,BANK_ACCOUNT_CURRENCY,USER_ID) values('10100','10000','EURO','10');
 
 insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account1@email',SHA('password1'), '1');
 insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account2@email',SHA('password2'), '2');
 insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account3@email',SHA('password3'), '3');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account4@email',SHA('password4'), '4');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account5@email',SHA('password5'), '5');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account6@email',SHA('password6'), '6');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account7@email',SHA('password7'), '7');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account8@email',SHA('password8'), '8');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account9@email',SHA('password9'), '9');
+insert into account(ACCOUNT_EMAIL,ACCOUNT_PASSWORD,USER_ID) values ('account10@email',SHA('password10'), '10');
 
-insert into contact(USER_ID_ACCOUNT,USER_ID_CONTACT) values ('1','2');
-insert into contact(USER_ID_ACCOUNT,USER_ID_CONTACT) values ('1','3');
-insert into contact(USER_ID_ACCOUNT,USER_ID_CONTACT) values ('2','1');
+
